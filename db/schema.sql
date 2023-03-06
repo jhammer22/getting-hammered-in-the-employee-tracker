b749d45fae2c65405e0fcb0cb6be0aa4e29b20cb
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-03-05 22:44:10 -0600

    schema done

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,34 @@
-DROP DATABASE IF EXISTS 
+DROP DATABASE IF EXISTS employee_db;
 
-CREATE DATABASE
+CREATE DATABASE employee_db;
 
-USE
+USE employee_db;
 
-CREATE TABLE
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  NAME VARCHAR(30)
+);
+
+CREATE TABLE ROLE (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role (id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+);
