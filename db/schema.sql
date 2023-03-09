8969666bde230b88356b9253d31be1a98b12527b
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-03-09 10:36:52 -0600

    schema fixed

--- db/schema.sql
+++ db/schema.sql
@@ -5,33 +5,32 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30),
-  PRIMARY KEY (id)
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
-CREATE TABLE ROLE (
-  role_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (role_id)
-  FOREIGN KEY department (id)
+  department_id INT UNSIGNED,
+  CONSTRAINT fk_department
+  FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-  employee_id INT NOT NULL AUTO_INCREMENT,
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INT NOT NULL AUTO_INCREMENT,
-  manager_id INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (employee_id)
+  role_id INT UNSIGNED NOT NULL,
+  CONSTRAINT fk_role
   FOREIGN KEY (role_id)
   REFERENCES role (id)
-  ON DELETE SET NULL
-  FOREIGN KEY (manager_id)
-  REFERENCES employee(id)
-  ON DELETE SET NULL
+  ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id)
+  REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -8,16 +8,16 @@ VALUES ("Manager"),
        ("Quality Control");
 
 INSERT INTO  role (role_id, title, salary, department_id)
-VALUES ("Regional Manager", 150000),
-       ("Sales III", 100000),
-       ("Assistant to the Regional Manager", 125000),
-       ("Accountant II", 75000),
-       ("Lead Quality Control", 65000);
+VALUES (01, "Regional Manager", 150000),
+       (02, "Sales III", 100000),
+       (03, "Assistant to the Regional Manager", 125000),
+       (04, "Accountant II", 75000),
+       (05, "Lead Quality Control", 65000);
 
 
-INSERT INTO employee (employee_id, first_name, last_name, role_id, manager_id)
-VALUES ("Michael", "Scott"),
-       ("Jim", "Halpert"),
-       ("Dwight", "Shrute"),
-       ("Kevin", "Malone"),
-       ("Creed", "Bratton");
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES ("Michael", "Scott", 01,),
+       ("Jim", "Halpert", 02,),
+       ("Dwight", "Shrute", 03,),
+       ("Kevin", "Malone", 04,),
+       ("Creed", "Bratton", 05,);
