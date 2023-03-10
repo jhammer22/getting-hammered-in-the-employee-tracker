c9d21363be1d78df71a27346abd34484c9ffd61e
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-03-09 22:28:14 -0600

    inquirer prompts and switch case written

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const express = require('express');
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const { createConnection } = require('net');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // connect to db
 const db = mysql.createConnection(
   {
     host: 'localhost',
     // MySQL username
-    user: '',
+    user: 'root',
   // MySQL password
     password: '',
     database: 'employee_db',
@@ -21,15 +19,45 @@ const db = mysql.createConnection(
   console.log(`database connected use with care`)
 );
 
+connection.connect(
+  console.log(`database connected use with care`)
+);
+
+async function startQuestions() {
+  try {
+    const question = await inquirer.prompt([{
+      type: 'list',
+      name: 'start',
+      message: 'Make a selection to get started',
+      choices: ['View Departments', 'Add Department', 'View Roles', 'Add Role', 'View Employees', 'Update Employee', 'Update Employee Role', 'Finished']
+    }]);
+    switch (question.choice) {
+      case 'View Departments':
+        return viewDepartments();
+        break;
+      case 'Add Department':
+        return addDepartment();
+        break;
+      case 'View Role':
+        return viewRole();
+        break;
+      case 'Add Role':
+        return addRole();
+        break;
+      case 'View Employees':
+        return viewEmployees();
+        break;
+      case 'Update Employee':
+        return updateEmployee();
+        break;
+      default:
+        return finished();             
+    }
+  } catch (err) {
+    console.log(err)
+  };
+};
 
 
 
 
-// default response for request not found
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`running the server is port ${PORT} is what you seek`)
-});
